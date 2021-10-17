638e3b46f0f7dcd92d8c76d29c59efa373100bbd
Abdul Jabbar Hafizh <jabbarworld1@gmail.com>
2021-10-17 20:15:27 +0700

    add protection for access API (admin and user)

--- routes/courseRoutes.js
+++ routes/courseRoutes.js
@@ -1,13 +1,38 @@
 const express = require('express');
 const router = express.Router();
 const courseController = require('../controllers/courseController');
+const authController = require('../controllers/authController');
 
-router.route('/').post(courseController.create).get(courseController.getAll);
+router
+  .route('/')
+  .post(
+    authController.protect,
+    authController.restrictTo('admin'),
+    courseController.create
+  )
+  .get(courseController.getAll);
 
-router.get('/statistics', courseController.getStats);
+router.get(
+  '/statistics',
+  authController.protect,
+  authController.restrictTo('admin'),
+  courseController.getStats
+);
 
-router.route('/:id').get(courseController.get).patch(courseController.update);
+router
+  .route('/:id')
+  .get(courseController.get)
+  .patch(
+    authController.protect,
+    authController.restrictTo('admin'),
+    courseController.update
+  );
 
-router.patch('/:id/delete', courseController.delete);
+router.patch(
+  '/:id/delete',
+  authController.protect,
+  authController.restrictTo('admin'),
+  courseController.delete
+);
 
 module.exports = router;
