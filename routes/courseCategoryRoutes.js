4140d422f40d982fedb6b2bd7ffe03ab19e9fcfa
Abdul Jabbar Hafizh <jabbarworld1@gmail.com>
2021-10-17 20:19:50 +0700

    define auth

--- routes/courseCategoryRoutes.js
+++ routes/courseCategoryRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const courseCategoryController = require('../controllers/courseCategoryController');
+const authController = require('../controllers/authController');
 
 router
   .route('/')
